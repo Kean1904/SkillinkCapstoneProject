02d1301d21aeb85aac2cb21c06347d548c4652b0
Kean torres <149301442+Kean1904@users.noreply.github.com>
2026-09-23 22:14:17 +0800

    Update services.php

--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
     ],
 
     'brevo' => [
-        'key' => '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',
+        'key' => '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',
         'sender_email' => 'torreskeanashleym2021@gmail.com',
     ],
 
