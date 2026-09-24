94d78061f21fc4889be2d252ac5dd484c7dbdd7b
Kean torres <149301442+Kean1904@users.noreply.github.com>
2026-09-24 15:24:27 +0800

    Update Admin.blade.php

--- resources/views/dashboard/Admin.blade.php
+++ resources/views/dashboard/Admin.blade.php
@@ -390,6 +390,7 @@
             display: flex;
             align-items: center;
             gap: 8px;
+            color: black;
         }
 
         /* CHARTS / PROGRESS CARDS */
@@ -729,7 +730,7 @@
 
             <!-- PERFORMANCE OVERVIEW (Matching Mobile) -->
             <div class="section-header">
-                <h3><i class="fa-solid fa-chart-line" style="color: black;"></i style="color: black;"> WEEKLY PERFORMANCE OVERVIEW</h3>
+                <h3><i class="fa-solid fa-chart-line" style="color: black;"></i> WEEKLY PERFORMANCE OVERVIEW</h3>
             </div>
 
             @php
@@ -826,7 +827,7 @@
 
             <!-- ADMINISTRATIVE MODULES SHORTCUT HUB -->
             <div class="section-header">
-                <h3><i class="fa-solid fa-cubes" style="color: black"></i style="color: black;"> ADMINISTRATIVE MANAGEMENT MODULES</h3>
+                <h3><i class="fa-solid fa-cubes" style="color: black"></i> ADMINISTRATIVE MANAGEMENT MODULES</h3>
             </div>
 
             <div class="modules-grid">
